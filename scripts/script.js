8402976b60c6ce2d33b1caa4d5c321feb77ce18d
ReineBeth <simplement.eli@hotmail.com>
2021-05-29 10:11:08 -0400

    modification sur le nom de certaines variables et changement de la fonction mettreAZeroIntervalle et intervallePausePlay, retirer des commentaires

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,15 @@
-// Je dois changer quelque chose pour pouvoir pousser
-// Initialisation des variables
+
 const boutonPrecedent = document.querySelector('.btn-precedent');
 const boutonSuivant = document.querySelector('.btn-suivant');
 const figuresCarrousel = document.querySelectorAll('figure');
-const nombresCarrousel = document.querySelectorAll('.nombre');
+const nombresCarrousel = document.querySelectorAll('.btn-nombre');
 const bodyElement = document.querySelector('body');
 const boutonPlayPause = document.querySelector('.btn-play-pause'); 
 const imagesCarrousel = document.querySelectorAll('img'); 
 
 let positionDuCarrousel = 0;
+let carrouselEnLecure = true; 
 let creerInterval = window.setInterval(naviguerVersImageSuivante, 3000);
-
-// Fonctions 
 
 function retirerClass() {
 	figuresCarrousel[positionDuCarrousel].classList.remove('visible');
@@ -46,24 +44,24 @@ function naviguerVersImagePrecedente() {
 	ajouterClass();
 }
 
-function mettreAZeroIntervalle() { 
-	window.clearInterval(creerInterval); 
-	if(boutonPlayPause.innerHTML === 'Pause') {
-		creerInterval = window.setInterval(naviguerVersImageSuivante, 3000);
-	} 
-}
 
 function intervallePausePlay() { 
-	if(boutonPlayPause.innerHTML === 'Jouer') { 
-		boutonPlayPause.innerHTML = 'Pause'; 
-	} else { 
+	if(carrouselEnLecure) { 
 		boutonPlayPause.innerHTML = 'Jouer'; 
+		carrouselEnLecure = false;  
+	} else { 
+		boutonPlayPause.innerHTML = 'Pause';
+		carrouselEnLecure = true; 
 	}
 	mettreAZeroIntervalle(); 
 }
 
-
-// Événements 
+function mettreAZeroIntervalle() { 
+	window.clearInterval(creerInterval); 
+	if(carrouselEnLecure) {
+		creerInterval = window.setInterval(naviguerVersImageSuivante, 3000);
+	} 
+}
 
 boutonSuivant.addEventListener('click', naviguerVersImageSuivante);
 
